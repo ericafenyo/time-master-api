7c0b4fdace8c196cfa399f3413a4583c9bc9cc10
ericafenyo <ericafenyo@yahoo.com>
2023-11-07 11:48:16 +0100

    feat: add time tracker

---
+++ src/main/java/com/ericafenyo/timemaster/timer/Timer.java
@@ -0,0 +1,58 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2023 Eric Afenyo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ericafenyo.timemaster.timer;
+
+import java.util.TimerTask;
+import java.util.function.Consumer;
+
+public class Timer {
+    private java.util.Timer timer;
+    private final String activityId;
+    private long startTime;
+
+    public Timer(String activityId) {
+        this.activityId = activityId;
+        this.timer = new java.util.Timer();
+    }
+
+    public void start(Consumer<Long> callback) {
+        startTime = System.currentTimeMillis();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Starting timer for activity with id: " + activityId);
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = currentTime - startTime;
+                callback.accept(elapsedTime);
+            }
+        };
+
+        timer.schedule(task, 1000, 1000);
+    }
+
+    public void stop() {
+        timer.cancel();
+    }
+}
